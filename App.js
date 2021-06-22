a4eb663c32b27a3800118be4d4eddd33ed8858a2
oliver <olivier.codes@gmail.com>
2021-06-22 19:24:18 +0200

    ajout readme

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// Librairies
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, Button } from 'react-native';
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{...styles.container, backgroundColor: darkMode ? '#ecf0f1' : '#2c3e50'}}>
+        {darkMode ?
+          <Image source={require('./assets/sun.png')} style={styles.logo}/>
+        :
+          <Image source={require('./assets/moon.png')} style={styles.logo}/>
+        }
+        <Button onPress={() => setDarkMode(darkMode => !darkMode)} title={darkMode ? 'mode jour' : 'mode nuit'}/>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 30,
+  }
 });
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # darkMode-react-native
+
+Application simple d'un dark mode au clic
